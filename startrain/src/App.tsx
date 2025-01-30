342497c5f49e8ada48a0d09b3e5c32ea607df87d
Pablo Campuzano Cuadrado <pbcampu@gmail.com>
2025-01-31 00:12:33 +0100

    a

---
+++ startrain/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [user, setUser] = useState(Number)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
