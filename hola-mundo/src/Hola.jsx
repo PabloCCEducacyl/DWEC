4289e79b275cd29c34ba06608879815419aeabf3
Pablo Campuzano Cuadrado <pablo.camcua@educa.jcyl.es>
2025-01-30 14:08:37 +0100

    .

--- hola-mundo/src/Hola.jsx
+++ hola-mundo/src/Hola.jsx
@@ -2,19 +2,9 @@ import { useEffect, useState } from 'react';
 import './Hola.css';
 
 export function Hola() {
-    const [bromomento, setMomento] = useState(String)
-
-    useEffect(() => {
-        async function granBromomento() {
-            const response = await fetch('http://geolocation-db.com/json/');
-            const data = await response.json();
-            setMomento(data.IPv4)
-        }
-        granBromomento()
-    })
     return (
         <h1 className='hola'>
-            Hola {bromomento}
+            Hola
         </h1>
     );
 }
